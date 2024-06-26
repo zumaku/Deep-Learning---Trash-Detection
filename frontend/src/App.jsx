e6ddbb3af3840ce537a94c170fc3991653c28b93
zumaku <60200121101@uin-alauddin.ac.id>
2024-06-26 16:47:32 +0800

    Update Tampilan

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { FaCamera, FaFileImage } from "react-icons/fa6";
 import Webcam from "react-webcam";
 import axios from "axios";
 
@@ -106,22 +107,30 @@ const App = () => {
 
     return (
         <div className="w-full p-10 flex flex-col items-center m-auto">
-            <h1 className="m-auto text-5xl font-bold mb-2 text-center">Trash Classification</h1>
-            <p className="mb-10">Dikembangkan Oleh <a href="#footer" className="font-bold hover:underline">Kelompok 2 - Kelas A</a></p>
+            <h1 className="m-auto text-5xl font-bold mb-10 text-center">
+                Trash
+                <span className="text-green-600"> Classification</span>
+            </h1>
 
-            <div className="flex flex-col justify-between items-center mb-10 gap-3">
-                <div className="flex flex-col justify-between w-100 sm:w-1/2 rounded-xl overflow-hidden">
+            <div className="w-full sm:w-3/4 flex flex-col sm:flex-row justify-between items-center sm:items-start mb-20 gap-4 sm:gap-6">
+                <div className="flex flex-col justify-between w-full sm:w-1/2 rounded-xl overflow-hidden">
                     <Webcam
                         audio={false}
                         ref={webcamRef}
                         screenshotFormat="image/jpeg"
-                        width={640}
-                        height={480}
+                        // width={640}
+                        // height={480}
                         className="mb-3 rounded-b-xl"
                     />
                     <div className="flex gap-2">
-                        <button onClick={capture} className="w-full rounded-xl font-bold py-3 text-lg bg-slate-50 hover:bg-slate-200 active:bg-slate-400 text-[#242424]">Capture</button>
-                        <button onClick={triggerFileInput} className="w-full rounded-xl font-bold py-3 text-lg bg-slate-50 hover:bg-slate-200 active:bg-slate-400 text-[#242424]">Upload</button>
+                        <button onClick={capture} className="w-full flex justify-center items-center gap-2 rounded-xl font-bold py-3 text-lg bg-green-600 hover:bg-green-700 active:bg-green-800 text-[#242424]">
+                            <FaCamera />
+                            <p>Capture</p>
+                        </button>
+                        <button onClick={triggerFileInput} className="w-full flex justify-center items-center gap-2 rounded-xl font-bold py-3 text-lg bg-slate-50 hover:bg-slate-200 active:bg-slate-400 text-[#242424]">
+                            <FaFileImage />
+                            <p>Upload</p>
+                        </button>
                         <input
                             type="file"
                             accept="image/*"
@@ -132,28 +141,38 @@ const App = () => {
                     </div>
                 </div>
 
-                <dir className="w-full h-0.5 bg-gray-400"></dir>
-
-                <div className="flex flex-col justify-between w-full rounded-xl overflow-hidden">
+                <div className="flex flex-col justify-between w-full sm:w-1/2 rounded-xl overflow-hidden">
                     {!isLoading && detectionResult ? (
                         <canvas
                             ref={canvasRef}
                             className="rounded-xl w-full"
                         />
                     ): (
-                        <div className="w-full p-4 aspect-video border-2 border-dashed border-gray-300 flex justify-center items-center rounded-xl mb-3">
+                        <div className="w-full p-4 aspect-video border-2 border-dashed border-green-500 flex justify-center items-center rounded-xl mb-3">
                             {isLoading ? (
-                                <p className="text-gray-300">Loading...</p>
+                                <p className="text-gray-300 font-semibold text-center">Loading...</p>
                             ):(
-                                <p className="text-gray-300">Hasil deteksi akan tampil disini...</p>
+                                <p className="text-gray-300 font-semibold text-center">Hasil deteksi akan tampil disini...</p>
                             )}
                         </div>
                     )}
                 </div>
             </div>
 
-            <p className="text-center">Dipersembahkan oleh Para Developer Hebar Kelas A</p>
-            <p className="font-bold text-center" id="footer">Ahmad Gazali | Amrul Ahsanullah | Rafiul Muiz | Fajri Ishar | Zul Fadli</p>
+            <h3 className="m-auto text-2xl font-bold text-center">
+                Tentang
+                <span className="text-green-500"> Aplikasi</span>
+            </h3>
+
+            <p className="text-center mb-2">Dalam rangka memenuhi tugas mata kuliah Deep Learning </p>
+            <p className="text-center mb-2">Dipersembahkan oleh Para Developer Hebat</p>
+            <div className="font-bold text-center" id="footer">
+                <a className="hover:underline hover:text-green-600" href="https://www.instagram.com/ahmadgazalizn/">Ahmad Gazali</a> | 
+                <a className="hover:underline hover:text-green-600" href="https://www.instagram.com/amrulahsanullah07/"> Amrul Ahsanullah</a> | 
+                <a className="hover:underline hover:text-green-600" href="https://www.instagram.com/rfiulmuizk/"> Rafiul Muiz</a> | 
+                <a className="hover:underline hover:text-green-600" href="https://www.instagram.com/fajriishar/"> Fajri Ishar</a> | 
+                <a className="hover:underline hover:text-green-600" href="https://www.instagram.com/fadliinlov3/"> Zul Fadli</a>
+            </div>
         </div>
     );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,9 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '760px',
+    }
   },
   plugins: [],
 }
